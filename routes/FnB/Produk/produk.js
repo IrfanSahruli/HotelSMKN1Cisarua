d17e11c3bf9c5ca7248f12712da3eb34607fe5a6
IrfanSahruli <irfansahruli244@gmail.com>
2024-11-21 15:02:08 +0700

    produk fnb

--- routes/FnB/Produk/produk.js
+++ routes/FnB/Produk/produk.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.post("/produk", upload.single("foto_produk"), protectApp(["admin"]), createProduk); //Tambah produk
 router.get("/produk", protectApp(["admin", "kasir"]), getAllProduk); //Get semua produk
-router.get("/produk/:kategori_produk", getProdukByKategori); //Get produk berdasarkan sub kategori
+router.get("/produk/:kategori_produk", protectApp(["kasir"]), getProdukByKategori); //Get produk berdasarkan kategori
 router.put("/produk/:id", upload.single("foto_produk"), protectApp(["admin"]), updateProduk); //Update produk berdasarkan id produk
 
 module.exports = router;
